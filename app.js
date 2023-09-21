5d3cf30d4967442f61f047fcc7a2efd4976a96d6
unknown <deepgarg157@gmail.com>
2023-09-22 01:13:43 +0530

    Food App through React JS

--- app.js
+++ app.js
@@ -48,20 +48,64 @@ import ReactDOM from "react-dom/client";
 // //   return <h1>Namaste React Functional Components</h1>;
 // // }
 
-const Element1 = () => <h1>Namaste React Functional components 1</h1>
+// const Element1 = () => <h1>Namaste React Functional components 1</h1>
 
-const Element2 = <h1>Namaste React Element</h1>
+// const Element2 = <h1>Namaste React Element</h1>
 
-const HeadingComponent2 = () => {
-  return <div>
-    {Element1()}
-    <Element1 />
-    <Element1></Element1>
-    {Element2}
-    <h1>Namaste React Functional Components</h1>
+// const HeadingComponent2 = () => {
+//   return <div>
+//     {Element1()}
+//     <Element1 />
+//     <Element1></Element1>
+//     {Element2}
+//     <h1>Namaste React Functional Components</h1>
+//   </div>
+// }
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(<HeadingComponent2 />)
+
+// Food App through React
+
+/**
+ * Header
+ * - Logo
+ * - Nav Items
+ * Body
+ * - Search
+ * - RestaurantContainer
+ * - RestaurantCard
+ * Footer
+ * - Copyright
+ * - Links
+ * - Address
+ * - Contact
+ */
+
+const Header = () => {
+  return(<div className="header">
+    <div className="logo-container">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdaMYtVi9_tfNcpsbGGseU6ehYgV9UeU3h7A&usqp=CAU"></img>
+    </div>
+    <div className="nav-items">
+     <ul>
+      <li>Home</li>
+      <li>About US</li>
+      <li>Contact Us</li>
+      <li>Cart</li>
+     </ul>
+    </div>
+
   </div>
+  )
 }
+
+const AppLayout = () => {
+  return(<div className="app">
+    <Header/>
+  </div>
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<HeadingComponent2 />)
 
-
+root.render(<AppLayout/>)
