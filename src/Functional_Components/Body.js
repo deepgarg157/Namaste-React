d3c3ecc343da4d8ef41d9b4455b2474fdd03bda5
unknown <deepgarg157@gmail.com>
2023-10-15 22:05:29 +0530

    React Js

--- src/Functional_Components/Body.js
+++ src/Functional_Components/Body.js
@@ -81,8 +81,7 @@ const Body = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6302312&lng=77.4349177&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
-    console.log(json)
-    // setListOfRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+    // console.log(json)
     setListOfRestaurants(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
     setFilterRest(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
   }
@@ -121,7 +120,7 @@ const Body = () => {
       <div className="res-container">
         {listRestaurrent.map((restaurant)=>(
           <Link
-          to="/restaurant/:resId"
+          to={"/restaurant/" + restaurant.info.id }
           key={restaurant.info.id}
           >
             {restaurant.info.promoted ? <RestaurrentCardPromoted resName ={restaurant} />  : (
--- src/Functional_Components/RestaurantMenu.js
+++ src/Functional_Components/RestaurantMenu.js
@@ -2,44 +2,33 @@ import { useEffect, useState } from "react";
 import resList from "../utils/mock_data";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
 
-    // const { resId } = useParams()
-
-    // const resInfo = useRestaurantMenu(resId)
-
-    const [resInfo, setResInfo] = useState(null);
-
-    useEffect(() => {
-        fetchData();
-      }, []);
-    
-      const fetchData = async () => {
-        const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6302312&lng=77.4349177&restaurantId=254002&catalog_qa=undefined&submitAction=ENTER");
-        const json = await data.json();
-        console.log(json)
-        setResInfo(json?.data);
-      };
+    const { resId } = useParams()
+    const resInfo = useRestaurantMenu(resId)
 
 
-      if (resInfo === null){
+    if (resInfo === null) {
         return <Shimmer />
-      }
- 
-      const {name, cuisines, avgRating, costForTwo} = resInfo.cards[0].card.card.info
- 
+    }
+
+    const { name, cuisines, avgRating, costForTwo } = resInfo.cards[0].card.card.info
+
+    const { itemCards } = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card
+
     return (
         <div>
             <h2>{name}</h2>
             <h3>{cuisines.join(", ")}</h3>
             <h4>{avgRating}</h4>
-            <h4>{costForTwo/100}</h4>
+            <h4>{costForTwo / 100}</h4>
             <h3>Menu</h3>
             <ul>
-                <li>Burger</li>
-                <li>Pizza</li>
-                <li>Chaap Masala</li>
+                {itemCards.map((item) =>
+                    <li key={item.card.info.id}>
+                        {item.card.info.name} - ${item.card.info.price / 100 || item.card.info.defaultPrice /100}</li>)}
             </ul>
         </div>
     )
@@ -47,15 +36,15 @@ const RestaurantMenu = () => {
 
 // High order components
 
-export const withPromotedRest = (RestaurantMenu)=>{
-   return ()=>{
-    return (
-        <div>
-            <label>Promoted</label>
-            <RestaurantMenu />
-        </div>
-    )
-   }
+export const withPromotedRest = (RestaurantMenu) => {
+    return () => {
+        return (
+            <div>
+                <label>Promoted</label>
+                <RestaurantMenu />
+            </div>
+        )
+    }
 }
 
 export default RestaurantMenu;
